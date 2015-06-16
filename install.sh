856015a506d5694c44a8710d42dd4dcbe10b645f
Luis Osa <luis.osa.gdc@gmail.com>
2015-06-16 23:17:01 +0200

    Vim: link bundle directory to dotfiles

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -siv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -siv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -siv "$DOTFILES_DIR/vim/.vimrc" ~
+ln -siv "$DOTFILES_DIR/vim/bundle" ~/.vim/bundle
