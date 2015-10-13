9adee577159398aa2422c8277f5c8da6a195b419
Luis Osa <luis.osa.gdc@gmail.com>
2015-10-13 14:42:00 +0200

    Expand installation: pathogen and submodule init

--- install.sh
+++ install.sh
@@ -5,6 +5,18 @@
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Install pathogen
+if [[ -a ~/.vim/autoload/pathogen.vim ]]; then
+	echo "Pathogen already installed; skipping"
+else
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+# Init submodules
+git submodule init && git submodule update
+
+# Symlink dotfiles
 ln -siv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -siv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -siv "$DOTFILES_DIR/vim/.vimrc" ~
