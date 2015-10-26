42624399ab100bad1c897905ff38d3dedf571b6e
Luis Osa <luis.osa.gdc@gmail.com>
2015-10-26 13:04:10 +0100

    Detab install script

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install pure prompt
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
-	echo "Prezto is installed; use its `prompt` function"
+    echo "Prezto is installed; use its `prompt` function"
 else
-	git clone git@github.com:sindresorhus/pure.git ~/pure
-	cd ~/pure
-       	ln -s "$PWD/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
-	ln -s "$PWD/async.zsh" /usr/local/share/zsh/site-functions/async
-	cd -
+    git clone git@github.com:sindresorhus/pure.git ~/pure
+    cd ~/pure
+    ln -s "$PWD/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
+    ln -s "$PWD/async.zsh" /usr/local/share/zsh/site-functions/async
+    cd -
 fi
 
 # Install pathogen
 if [[ -a ~/.vim/autoload/pathogen.vim ]]; then
-	echo "Pathogen already installed; skipping"
+    echo "Pathogen already installed; skipping"
 else
-	mkdir -p ~/.vim/autoload && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    mkdir -p ~/.vim/autoload && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
 # Init submodules
